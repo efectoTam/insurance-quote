72fab79fb606e120ffeb56f8eca8e310d6acd576
Tamara Luque <tam.luque@gmail.com>
2020-08-31 23:32:13 -0400

    more structures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+const Container = styled.div `
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header
-      title="Cotizador de seguros"
-    />
+    <Container>
+      <Header
+        title="Cotizador de seguros"
+      />
+      <FormContainer>
+        
+      </FormContainer>
+    </Container>
   );
 }
 
